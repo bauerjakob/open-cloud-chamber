36fad5e6cd74776e3164d76d5689af7e8de6fa03
Jakob <mail@bauer-jakob.de>
2024-04-28 20:11:01 +0200

    Initial Commit

---
+++ OpenCloudChamber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenCloudChamber
+//
+//  Created by Jakob Bauer on 28.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenCloudChamber/OpenCloudChamberApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCloudChamberApp.swift
+//  OpenCloudChamber
+//
+//  Created by Jakob Bauer on 28.04.24.
+//
+
+import SwiftUI
+
+@main
+struct OpenCloudChamberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
